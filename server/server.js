3ea7f3809951abefec7a7ec4f91c36dcaffaf008
Heavyrisem <pyo1748@naver.com>
2020-12-20 00:11:09 +0900

    서버: 세기말 변동

--- server/server.js
+++ server/server.js
@@ -86,12 +86,12 @@ function calculateCoinValue() {
     // rand = DefaultCoinValue RandomData()
     switch (RandomData(0, 2)) {
         case 0: {
-            rand = Math.abs(Math.sin(coinvalue) * RandomData(40, 81) * 100);
+            rand = Math.abs(Math.sin(coinvalue) * RandomData(40, 81) * 200);
             type = "sin";
             break;
         }
         case 1: {
-            rand = Math.abs(Math.tan(coinvalue) * RandomData(30, 51) * 100);
+            rand = Math.abs(Math.tan(coinvalue) * RandomData(30, 51) * 200);
             type = "tan";
             break;
         }
