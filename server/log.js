28a4d08a092c3b69eb6af71a59b0b5246b675a7f
Heavyrisem <pyo1748@naver.com>
2020-12-19 22:29:09 +0900

    서버: 데이터베이스 변경

--- server/log.js
+++ server/log.js
@@ -1,11 +1,16 @@
-const sqlite3 = require('sqlite3');
-const DB = new sqlite3.Database('./DB.db', sqlite3.OPEN_READWRITE, err => {
-    if (err) {
-        console.log(`Error while Opening DB ${err}`);
-    } else {
-        console.log(`Database Connected`);
-    }
-});
+// const sqlite3 = require('sqlite3');
+// const DB = new sqlite3.Database('./DB.db', sqlite3.OPEN_READWRITE, err => {
+//     if (err) {
+//         console.log(`Error while Opening DB ${err}`);
+//     } else {
+//         console.log(`Database Connected`);
+//     }
+// });
+const Config = require('./Config.json');
+const mysql = require('mysql');
+const DB = mysql.createConnection(Config.DB);
+DB.connect();
+
 
 function GetCurrentTime() {
     let now = new Date();
@@ -20,7 +25,7 @@ function writeLog(Author, Type, Message) {
     }
             
     console.log(`[${GetCurrentTime()}] [${Type}] [${Author}] ${Message}`);
-    DB.run(`INSERT INTO Log(Date, Message, Type, Author) VALUES('${new Date()}', '${Message}', '${Type}', '${Author}')`);
+    DB.query(`INSERT INTO Log(Date, Message, Type, Author) VALUES('${new Date()}', '${Message}', '${Type}', '${Author}')`);
 }
 
 function replaceAll(originstr, findstr, replacestr) {
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const Config = require("./Config.json")
+
 const express = require('express');
 const app = express();
 // require('greenlock-express').init({
@@ -13,14 +15,26 @@ const fs = require('fs');
 
 const Log = require('./log');
 
-const sqlite3 = require('sqlite3').verbose();
-const DB = new sqlite3.Database('./DB.db', sqlite3.OPEN_READWRITE, err => { // d
-    if (err) {
-        console.log(`Error while Opening DB ${err}`);
-    } else {
-        console.log(`Database Connected`);
-    }
-});
+const mysql = require('mysql');
+const DB = mysql.createConnection(Config.DB);
+DB.connect();
+
+DB.query('SELECT * FROM userinfo', (err, rows) => {
+    if (err) return console.log(err);
+    rows.forEach(value => {
+        console.log(value.name);
+    })
+    // console.log(rows);
+})
+
+// const sqlite3 = require('sqlite3').verbose();
+// const DB = new sqlite3.Database('./DB.db', sqlite3.OPEN_READWRITE, err => { // d
+//     if (err) {
+//         console.log(`Error while Opening DB ${err}`);
+//     } else {
+//         console.log(`Database Connected`);
+//     }
+// });
 
 app.use(express.static('../build'));
 app.use(require('cors')());
@@ -40,7 +54,7 @@ let coinvalue = 0;
 io.on("connection", client => {
 
     // console.log(`${client.id} 님이 접속했습니다.`);
-    DB.all(`SELECT * FROM coinValue`, (err, rows) => {
+    DB.query(`SELECT * FROM coinValue`, (err, rows) => {
         if (err) return Log.writeLog("System", "Error", "coinValue 데이터베이스 조회 오류");
         if (!rows.length) return;
 
@@ -49,14 +63,6 @@ io.on("connection", client => {
             let date = new Date(rows[rows.length-1-i].date);
             io.emit("CoinValue", { coinValue: rows[rows.length-1-i].value, updateTime: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`, nextUpdate: UpdateTick, type: "none" });
             // console.log(rows[rows.length-1-i]);
-        }
-    })
-
-    client.on("login", (data) => {
-        if (data) {
-            DB.get("SELECT * FROM userinfo WHERE name='${data.name}'", (err, row) => {
-                console.log(row);
-            });
         }
     })
 
@@ -124,7 +130,7 @@ function calculateCoinValue(value) {
     coinvalue = rand;
 
     io.emit("CoinValue", { coinValue: coinvalue, updateTime: `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`, nextUpdate: UpdateTick, type: type });
-    DB.run(`INSERT INTO coinValue(value, date) VALUES('${coinvalue}', '${now}')`);
+    DB.query(`INSERT INTO coinValue(value, date) VALUES('${coinvalue}', '${now}')`);
     setTimeout(calculateCoinValue, UpdateTick);
 
 }
@@ -151,8 +157,10 @@ app.post("/login", (req, res) => {
         Log.writeLog("System", "Information", "빈 데이터로 로그인 시도 " + req.headers['x-forwarded-for'] || req.connection.remoteAddress);
         return res.send({ msg: "WRONG_DATA" });
     }
-    DB.get(`SELECT * FROM userinfo WHERE name='${req.body.id}'`, (err, row) => {
+    DB.query(`SELECT * FROM userinfo WHERE name='${req.body.id}'`, (err, row) => {
         if (err)  { res.send({msg: "데이터베이스 조회 오류"}); return Log.writeLog(req.body.id, "Error", "데이터베이스 조회 오류" + err) }
+        if (row.length == 0) row = undefined;
+        else row = row[0];
         if (row) {
             if (row.passwd == SHA256(req.body.passwd)) {
                 res.send({ id: row.name, CoinBalance: row.CoinBalance, MoneyBalance: row.MoneyBalance });
@@ -173,13 +181,15 @@ app.post("/register", (req, res) => {
         Log.writeLog("System", "Information", "빈 데이터로 회원가입 시도 " + req.headers['x-forwarded-for'] || req.connection.remoteAddress);
         return res.send({ msg: "WRONG_DATA" });
     }
-    DB.get(`SELECT * FROM userinfo WHERE name='${req.body.id}'`, (err, row) => {
+    DB.query(`SELECT * FROM userinfo WHERE name='${req.body.id}'`, (err, row) => {
         if (err)  { res.send({msg: "데이터베이스 조회 오류"}); return Log.writeLog(req.body.id, "Error", "데이터베이스 조회 오류" + err) }
+        if (row.length == 0) row = undefined;
+        else row = row[0];
         if (row) {
             Log.writeLog("System", "Information", `${row.name} 사용자 중복 회원가입 시도 ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`);
             return res.send({ msg: "USER_EXISTS" });
         } else {
-            DB.run(`INSERT INTO userinfo(name, passwd, CoinBalance, MoneyBalance) VALUES("${req.body.id}", "${SHA256(req.body.passwd)}", "100", "300000")`, (err, row) => {
+            DB.query(`INSERT INTO userinfo(name, passwd, CoinBalance, MoneyBalance) VALUES("${req.body.id}", "${SHA256(req.body.passwd)}", "100", "300000")`, (err, row) => {
                 if (err)  { res.send({msg: "데이터베이스 조회 오류"}); return Log.writeLog(req.body.id, "Error", "데이터베이스 조회 오류" + err) }
                 Log.writeLog("System", "Information", `${req.body.id} 회원가입 성공 ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`);
                 return res.send({ id: req.body.id });
@@ -193,14 +203,16 @@ app.post("/buy", (req, res) => {
         Log.writeLog("System", "Trade", "빈 데이터로 구매 시도 " + req.headers['x-forwarded-for'] || req.connection.remoteAddress);
         return res.send({ msg: "WRONG_DATA" });
     }
-    DB.get(`SELECT * FROM userinfo WHERE name='${req.body.id}'`, (err, row) => {
+    DB.query(`SELECT * FROM userinfo WHERE name='${req.body.id}'`, (err, row) => {
         if (err)  { res.send({msg: "데이터베이스 조회 오류"}); return Log.writeLog(req.body.id, "Error", "데이터베이스 조회 오류" + err) }
+        if (row.length == 0) row = undefined;
+        else row = row[0];
         
         if (row) {
 
             let preCal = parseInt(coinvalue) * parseInt(req.body.Amount);
             if (parseInt(row.MoneyBalance) >= preCal) {
-                DB.run(`UPDATE userinfo SET MoneyBalance=${parseInt(row.MoneyBalance) - preCal}, CoinBalance=${parseInt(row.CoinBalance) + parseInt(req.body.Amount)} WHERE name='${req.body.id}'`, err => {
+                DB.query(`UPDATE userinfo SET MoneyBalance=${parseInt(row.MoneyBalance) - preCal}, CoinBalance=${parseInt(row.CoinBalance) + parseInt(req.body.Amount)} WHERE name='${req.body.id}'`, err => {
                     if (err)  { res.send({msg: "데이터베이스 조회 오류"}); return Log.writeLog(req.body.id, "Error", "데이터베이스 조회 오류" + err) }
 
                     Log.writeLog(req.body.id, "Trade", `${row.name} ${req.body.Amount} 코인 구매, 잔액 ${parseInt(row.CoinBalance) + parseInt(req.body.Amount)}`);
@@ -224,14 +236,16 @@ app.post("/sell", (req, res) => {
         Log.writeLog("System", "Trade", "빈 데이터로 판매 시도 " + req.headers['x-forwarded-for'] || req.connection.remoteAddress);
         return res.send({ msg: "WRONG_DATA" });
     }
-    DB.get(`SELECT * FROM userinfo WHERE name='${req.body.id}'`, (err, row) => {
+    DB.query(`SELECT * FROM userinfo WHERE name='${req.body.id}'`, (err, row) => {
         if (err)  { res.send({msg: "데이터베이스 조회 오류"}); return Log.writeLog(req.body.id, "Error", "데이터베이스 조회 오류" + err) }
+        if (row.length == 0) row = undefined;
+        else row = row[0];
         if (row) {
 
             let preCal = coinvalue * req.body.Amount;
 
             if (parseInt(row.CoinBalance) >= parseInt(req.body.Amount)) {
-                DB.run(`UPDATE userinfo SET MoneyBalance=${parseInt(row.MoneyBalance) + preCal}, CoinBalance=${parseInt(row.CoinBalance) - parseInt(req.body.Amount)} WHERE name='${req.body.id}'`, err => {
+                DB.query(`UPDATE userinfo SET MoneyBalance=${parseInt(row.MoneyBalance) + preCal}, CoinBalance=${parseInt(row.CoinBalance) - parseInt(req.body.Amount)} WHERE name='${req.body.id}'`, err => {
                     if (err)  { res.send({msg: "데이터베이스 조회 오류"}); return Log.writeLog(req.body.id, "Error", "데이터베이스 조회 오류" + err) }
                     Log.writeLog(req.body.id, "Trade", `${row.name} ${req.body.Amount} 코인 판매, 잔액 ${parseInt(row.MoneyBalance) + preCal}`);
                     return res.send({ CoinBalance: parseInt(row.CoinBalance) - parseInt(req.body.Amount), MoneyBalance: parseInt(row.MoneyBalance) + preCal });
@@ -249,7 +263,7 @@ app.post("/sell", (req, res) => {
 })
 
 app.post("/getWise", (req, res) => {
-    DB.all(`SELECT * FROM wiseSaying`, (err, rows) => {
+    DB.query(`SELECT * FROM wiseSaying`, (err, rows) => {
         if (err) return console.log(err);
         if (rows.length) {
             let n = Math.floor(Math.random() * (rows.length - 0));
@@ -263,10 +277,10 @@ app.post("/getWise", (req, res) => {
 // Develope Tools
 
 app.post("/wise", (req, res) => {
-    DB.get(`SELECT * FROM wiseSaying WHERE Message='${req.body.Message}'`, (err, row) => {
+    DB.query(`SELECT * FROM wiseSaying WHERE Message='${req.body.Message}'`, (err, row) => {
         if (row) return res.send("ALREADY EXISTS");
 
-        DB.run(`INSERT INTO wiseSaying(Author, Message) VALUES('${req.body.Author}', '${req.body.Message}')`, err => {
+        DB.query(`INSERT INTO wiseSaying(Author, Message) VALUES('${req.body.Author}', '${req.body.Message}')`, err => {
             if (err) return console.log(err);
         });
         res.send("OK");
@@ -294,15 +308,17 @@ app.post("/getLog", (req, res) => {
         return;
     }
     Log.writeLog("Admin", "GetLog", `로그 조회 ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`);
-    DB.all(`SELECT * FROM Log`, (err, rows) => {
+    DB.query(`SELECT * FROM Log`, (err, rows) => {
         if (err)  { res.send({msg: "데이터베이스 조회 오류"}); return Log.writeLog(req.body.id, "Error", "데이터베이스 조회 오류" + err) }
         res.send(rows);
     })
 })
 
 server.listen(PORT, () => {
-    DB.get("SELECT * FROM coinValue where id=(SELECT MAX(id) FROM coinValue)", (err, row) => {
+    DB.query("SELECT * FROM coinValue where id=(SELECT MAX(id) FROM coinValue)", (err, row) => {
         if (err) return Log.writeLog("System", "Error", "Error while Get Last Value", err);
+        if (row.length == 0) row = undefined;
+        else row = row[0];
 
         if (row) {
             coinvalue = row.value;
@@ -313,3 +329,57 @@ server.listen(PORT, () => {
         calculateCoinValue();
     })
 });
+
+// CREATE TABLE Log (
+// 	id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+// 	Date TEXT NOT NULL,
+// 	Message TEXT NOT NULL,
+// 	Type TEXT NOT NULL,
+// 	Author TEXT NOT NULL
+// );
+
+// CREATE TABLE coinValue (
+// 	id INTEGER AUTO_INCREMENT PRIMARY KEY UNIQUE,
+// 	value TEXT NOT NULL,
+// 	date INTEGER NOT NULL
+// );
+
+// CREATE TABLE userinfo (
+// 	id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+// 	name TEXT NOT NULL,
+// 	passwd TEXT NOT NULL,
+// 	CoinBalance INTEGER NOT NULL,
+// 	MoneyBalance INTEGER NOT NULL
+// );
+
+// CREATE TABLE wiseSaying (
+// 	id  INTEGER NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+// 	Author  TEXT NOT NULL,
+// 	Message TEXT NOT NULL
+// );
+
+// INSERT INTO wiseSaying(Author, Message) VALUES('안중근', '내가 죽은 뒤에 나의 뼈를 하얼빈 공원 옆에 묻어 두었다가 나라를 되찾거든 고국으로 옮겨 다오. 나는 천국에 가서도 마땅히 우리나라의 독립을 위해 힘쓸 것이다. 대한 독립의 소리가 천국에 들려오면 나는 마땅히 춤추며 만세를 부를 것이다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('안중근', '이익을 보거든 정의를 생각하고, 위태로움을 보거든 목숨을 바쳐라.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('안창호', '진실은 반드시 따르는 자가 있고, 정의는 반드시 이루는 날이 있다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('이봉창 ', '인생의 목적이 쾌락이라면 지난 31년 동안에 쾌락이란 것을 대강 맛을 보았습니다. 이제부터는 영원한 쾌락을 위해서 독립 사업에 몸을 바칠 각오로 상해에 왔습니다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('김구 ', '우리 민족의 지나간 역사가 빛나지 아니함이 아니나, 그것은 아직 서곡이었다. 우리가 주연 배우로 세계 역사의 무대에 나서는 것은 오늘 이후다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('유관순 ', '나는 왜놈 따위에게 굴복하지 않는다! 언젠가 네놈들은 반드시 천벌을 받고 반드시 망하게 되리라!');
+// INSERT INTO wiseSaying(Author, Message) VALUES('윤봉길 ', '죽음을 택해야 할 오직 한 번의 가장 좋은 기회를 포착했습니다. 백년을 살기보다 조국의 영광을 지키는 이 기회를 택했습니다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('조마리아 ', '집안 일은 생각하지 말고 최후까지 남자답게 싸워라.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('이범석 ', '한 말 배는 산산조각이 나고 몸은 갈기갈기 찢어지는 한이 있더라도 나의 혼과 정열은 한 방울의 피 한 점의 살이라도 내 사랑하는 조국 땅에 뿌려주고 던져 줄 것을 나는 확신한다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('강우규', '내가 죽어서 청년들의 가슴에 조그마한 충격이라도 줄 수 있다면 그것은 내가 소원하는 일이다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('김좌진 ', '할 일이... 할 일이 너무도 많은 이 때에 내가 죽어야 하다니, 그게 한스러워서...');
+// INSERT INTO wiseSaying(Author, Message) VALUES('이상룡', '공자와 맹자는 나라를 되찾은 뒤 읽어도 늦지 않다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('신채호', '역사를 잊은 민족에게 미래는 없다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('유관순 ', '내 손톱이 빠져나가고, 내 귀와 코가 잘리고,  내 손과 다리가 부러져도 그 고통은 이길 수 있사오나,  나라를 잃어버린 그 고통만은 견딜 수가 없습니다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('윤봉길', '대장부는 집을 나가 뜻을 이루기 전에는  살아 돌아오지 않는다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('안중근', '나는 국민의 의무로서 내 몸을 희생하여  어진 일을 이루고자 했을 뿐이다.  내 이미 죽음을 각오하고 결행한 바이니  죽어도 여한이 없노라.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('김구', '눈길을 걸어갈 때 어지럽게 걷지 말기를,  오늘 내가 걸어간 길이   훗날 다른 사람의 이정표가 되리니');
+// INSERT INTO wiseSaying(Author, Message) VALUES('유관순 ', '내 손과 다리가 부러져도 그 고통은 이길 수 있사오나  나라를 잃어버린 그 고통만은 견딜 수가 없습니다.  ​  나라에 바칠 목숨이 오직 하나밖에 없는 것만이  이 소녀의 유일한 슬픔입니다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('안중근 ', '뿌리 없는 나무가  어디에서 날 것이며,  ​  나라 없는 백성이  어디서 살 것인가');
+// INSERT INTO wiseSaying(Author, Message) VALUES('안중근', '나를 죄인 취급하지 말아라.  ​  이토록 죽인 것은 나 일개인을 위한 것이 아닌  동양 평화를 위한 것이다.  ​  나는 독립군의 중장 자격으로 이토를 살해했다.  ​  나는 군인이다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('안창호', '나는 밥을 먹어도 대한의 독립을 위해  잠을 자도 대한의 독립을 위해 해왔다.  ​  이것은 내 목숨이 없어질 때까지 변함이 없을 것이다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('안창호', '낙망은 청년의 죽음이요,  청년이 죽으면 민족이 죽는다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('안창호', '견고한 기초 위에 좋은 건설이 있고,  튼튼한 뿌리 위에 좋은 꽃과 열매가 있다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('안창호', '자기 몸과 집을 자신이 다스리지 않으면  대신 다스려줄 사람이 없듯이  ​  자기 국가와 민족을 자신이 구하지 않으면  구해줄 사람이 없다는 것을 아는 것이  바로 책임감이요, 주인 관념이다.');
+// INSERT INTO wiseSaying(Author, Message) VALUES('안창호', '그대는 매일 5분씩이라도  나라를 생각해 본 일이 있는가?');
