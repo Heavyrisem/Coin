043e7e4a065a839ff9ad6f9f827e12cf49d46700
Heavyrisem <pyo1748@naver.com>
2020-12-20 01:53:52 +0900

    서버: 세기말 변동

--- server/Reset.js
+++ server/Reset.js
@@ -8,10 +8,14 @@ DB.query("select * from userinfo", (err, rows) => {
 
     rows.forEach(value => {
         if (value.MoneyBalance >= 50000000 || value.CoinBalance > 200) {
+            let money = 50000000;
+            let coin = 0;
+            if (value.CoinBalance > 0) money += 10000000;
 
-            console.log(value.name, value.CoinBalance, value.MoneyBalance);
+            DB.query(`UPDATE userinfo set MoneyBalance=${money}, CoinBalance=${coin} WHERE name='${value.name}'`, (err) => {return console.log(err)});
+            console.log("초기화 대상 : ", value.name, value.CoinBalance, value.MoneyBalance);
         }
     })
     
-    DB.destroy();
+    // DB.destroy();
 });
