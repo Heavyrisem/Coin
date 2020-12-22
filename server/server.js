ee9747db258e5a502c57f0ffe56b9f0e01745421
Heavyrisem <pyo1748@naver.com>
2020-12-22 22:23:53 +0900

    크롤링 차단

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ const DefaultCoinValue = 100000;
 const MaximunCoinValue = 1000000;
 let coinvalue = 0;
 
-const Client_VER = 1;
+const Client_VER = 2;
 
 app.use((req, res, next) => {
     if (!req.url.indexOf("index.html") == -1 || req.url != '/') return next();
