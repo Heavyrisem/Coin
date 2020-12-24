4ba33a70e7c388ab588381f1e9519d986da93d79
Heavyrisem <pyo1748@naver.com>
2020-12-24 12:22:11 +0900

    서버: 특정 아이피 허용

--- server/Middle/BlockCounty.js
+++ server/Middle/BlockCounty.js
@@ -8,14 +8,13 @@ function BlockCountry(req, res, next) {
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     let geoipInfo = geoip.lookup(ip);
 
-    console.log(ip, typeof ip);
     if (ALLOWLIST.indexOf(ip) != -1) {
         Log.writeLog("System", "AllowOtherCountry", `${geoipInfo.country} is Allowed By ALLOWLIST`, ip);
         return next();
     }
 
     if (geoipInfo != undefined && ALLOWLIST.indexOf(geoipInfo.country) == -1) {
-        Log.writeLog("System", "BlockOtherCountry", `${geoipInfo.country} is Blocked By ExpreeMiddleWare`, ip);
+        Log.writeLog("System", "BlockOtherCountry", `${geoipInfo.country} is Blocked By ExpreeMiddleWare, ${(req.header('User-Agent'))&& req.header('User-Agent')}`, ip);
         return;
     } else {
         if (geoipInfo != undefined && geoipInfo.country != 'KR') Log.writeLog("System", "CountryInfo", `${geoipInfo.country}`, ip);
