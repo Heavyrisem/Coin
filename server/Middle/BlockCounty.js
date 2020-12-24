a00d8379893547a9d1e5b5a3efafbcbed5a6ad57
Heavyrisem <pyo1748@naver.com>
2020-12-24 12:17:46 +0900

    서버: 특정 아이피 허용

--- server/Middle/BlockCounty.js
+++ server/Middle/BlockCounty.js
@@ -2,13 +2,14 @@ const geoip = require('geoip-country');
 const Log = require('../log');
 
 const BLACKLIST = ['CN', 'TW'];
-const ALLOWLIST = ['KR', '35.237.4.214', '35.196.132.85'];
+const ALLOWLIST = ['KR', '::ffff:35.237.4.214', '::ffff:35.196.132.85'];
 
 function BlockCountry(req, res, next) {
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     let geoipInfo = geoip.lookup(ip);
 
-    if (ALLOWLIST.indexOf(ip+"".replace("::ffff:", "")) != -1) {
+    console.log(ip, typeof ip);
+    if (ALLOWLIST.indexOf(ip) != -1) {
         Log.writeLog("System", "AllowOtherCountry", `${geoipInfo.country} is Allowed By ALLOWLIST`, ip);
         return next();
     }
