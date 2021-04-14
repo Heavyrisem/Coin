022fb151325196b0288199cfb6c278b4f7b1bf92
heavyrisem <pyo1748@gmail.com>
2021-04-14 16:08:21 +0900

    그래프 수정

--- server/server.js
+++ server/server.js
@@ -135,7 +135,7 @@ function calculateCoinValue() {
     let rand = 0;
     // let type;
 
-    let NextDir = (coinvalue <= DefaultCoinValue) ? RandomData(0, 5) : RandomData(0, 2); // true is UP false is DOWN
+    let NextDir = (coinvalue <= DefaultCoinValue) ? RandomData(0, 10) : RandomData(0, 2); // true is UP false is DOWN
     let Perc = Math.floor(Math.random() * (50 - 3)) + 3; // %
     console.log((NextDir)? (parseInt(coinvalue * Perc / 100)) : (parseInt(-(coinvalue * Perc / 100))));
     coinvalue = parseInt(coinvalue);
